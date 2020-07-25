b6a7ae91d7057cd9dc796bc25edce8ac4ba934c8
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-25 23:32:11 +0000

    Added article page template

---
+++ src/components/UnmaskArticle.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="unmask-article">
+    <div class="article-encapsulation">
+      <h1>{{ article.title }}</h1>
+      <!--Video for the audiowave-->
+      <!--<video src=""></video>--> 
+    </div>
+    <p>{{ article.typedjs }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'UnmaskArticle',
+  props: {
+    article: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+@import "~@/assets/scss/_typo.scss";
+</style>
