ed5f9c2cca04691a7c0e9784d09372b12ffee56c
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-27 03:46:19 +0000

    Added backend communication. Stores tags and countries in Vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes.js'
 import { store } from './store/store.js'
+import axios from './articleAPI.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
+    mounted: function() {
+        this.getTags();
+        this.getCountries();
+    },
+    methods: {
+        getTags: function() { axios().get('/tags/').then(response => this.$store.state.tags = response.data) },
+        getCountries: function() { axios().get('/countries/').then(response => this.$store.state.countries = response.data) },
+    },
     render: h => h(App),
 }).$mount('#app')
