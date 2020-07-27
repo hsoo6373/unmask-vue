f8aaa72706ac874f784cda68b09e823213c139d8
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-27 01:00:42 +0000

    Updated landing page path

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ Vue.use(VueRouter)
 export const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '', component: LandingPage },
+        { path: '/', component: LandingPage },
         { path: '/tags', component: TagPage },
         { path: '/articles', component: UnmaskArticle },
     ]
