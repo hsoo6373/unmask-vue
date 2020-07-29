a9decfc6cf4f63ace60f471d3cf767eb76b7a0f5
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-07-29 09:06:38 +0000

    Implemented 'infinite scroll' added sass styling to the tag page

--- src/components/TagPage.vue
+++ src/components/TagPage.vue
@@ -1,30 +1,126 @@
 <template>
   <div class="tag-page">
-    <ul>
-      <li 
-        v-for="tag in tags" 
-        :key="tag.tag"
-      >
-        {{ tag.tag }}
+    <template v-for="repeat in infiniteCounter">
+    <ul :key="repeat">
+      <h1 :style="{ fontSize: '4em'}">{{ repeat }}</h1>
+      <li v-for="(item, index) in collection" :key="`${repeat} - ${item.name}`">
+        <span :class="assignClass(index, repeat)">{{ item.name }}</span>
       </li>
     </ul>
+    </template>
+    <observer @intersect="intersecting"></observer>
   </div>
 </template>
 
 <script>
+import Observer from './Observer';
+
 export default {
   name: 'TagPage',
+  data: function() {
+    return {
+      infiniteCounter: 5,
+    }
+  },
+  components: {
+    Observer,
+  },
+  methods: {
+    assignClass: function(index, repeat) {
+      return {
+        evenRepeatEven: index % 2 === 0 && repeat % 2 === 0,
+        evenRepeatOdd: index % 2 === 1 && repeat % 2 === 0,
+        oddRepeatEven: index % 2 === 0 && repeat % 2 === 1,
+        oddRepeatOdd: index % 2 === 1 && repeat % 2 === 1,
+      }
+    },
+    intersecting: function() {
+      this.infiniteCounter++;
+    }
+  },
+  computed: {
+    collection: function() {
+      return this.$store.getters.getCollection;
+    },
+    check: function() {
+      console.log(" Visible");
+      
+    }
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='scss'>
 @import "~@/assets/scss/_typo.scss";
 
-h1 {
-  font-size: 3em;
-  font-family: CustomY78;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.tag-page {
+  font-family: "CustomY78";
+  width: 90%;
 }
+
+ul {
+  background: none;
+  list-style: none;
+  font-size: 2em;
+  display: flex;
+  flex-direction: rows;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  position: relative;
+  
+  h1 {
+    font-size: 2em;
+    width: 100%;
+    text-align: center;
+    color: #302f2f;
+    z-index: 1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  
+  li { 
+    margin: 4% 2%;
+    z-index: 2;
+    background: none;
+    
+    .oddRepeatEven {
+      color: white;
+      &::selection {
+        color: purple;
+      }
+    }
+    
+    .oddRepeatOdd {
+      color: yellow;
+      &::selection {
+        color: red;
+      }
+    }
+    
+    .evenRepeatEven {
+      color: purple;
+      &::selection {
+        color: white;
+      }
+    }
+    
+    .evenRepeatOdd {
+      color: red;
+      &::selection {
+        color: yellow;
+      }
+    }
+    
+    span {
+      background: none;
+      z-index: 2;
+      display: block;
+      text-align: center;
+    }
+    
+  }
+}
+
 </style>
