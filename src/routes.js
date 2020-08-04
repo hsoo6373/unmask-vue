f8a323f7a24ad97108a393be5f94fdf3442f3281
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-08-04 11:30:47 +0000

    Created new routes. ArticlePage is removed. Reusing TagPage template

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LandingPage from './components/LandingPage.vue'
 import TagPage from './components/TagPage.vue'
-import ArticlePage from './components/ArticlePage.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +11,7 @@ export const router = new VueRouter({
         { name: 'home', path: '/', component: LandingPage },
         { name: 'tags', path: '/tags', component: TagPage },
         { name: 'tag', path: '/tags/:tag', component: TagPage },
-        { name: 'article', path: '/tags/:tag-name/:article', component: TagPage },
+        { name: 'article', path: '/tags/:tag/:article', component: TagPage },
         { path: '*', redirect: { name: 'home' } },
     ]
 });
