16261e897a7edb9ffae40fb8c3f6497c4367d019
hsoo6373 <hsoo6373@uni.sydney.edu.au>
2020-08-07 14:30:45 +0000

    Added vuetify plugin

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes.js'
 import { store } from './store/store.js'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
-    render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
